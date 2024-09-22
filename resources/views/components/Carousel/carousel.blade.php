7e1c7e7467fa84975d340de01f532c5855ba29f1
azmayusuf <azmayusuf@outlook.com>
2024-09-22 23:53:19 +0700

    Melakukan penambahan codingan pada file navbar, carousel, dan index blade sebagai induk nya dari frontend

--- resources/views/components/Carousel/carousel.blade.php
+++ resources/views/components/Carousel/carousel.blade.php
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
-</body>
-</html>
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="1000">
+        <img src="https://th.bing.com/th/id/OIP.hKF-QDQbOe-0GEQyJoHfGAHaE8?rs=1&pid=ImgDetMain" class="d-block w-50" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="1000">
+        <img src="https://images.squarespace-cdn.com/content/v1/53081924e4b0dbc78d13d4aa/1549333015501-DYXON2TGDWV4EQPLFK3W/butterfly6.jpg" class="d-block w-50" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="1000">
+        <img src="https://th.bing.com/th/id/OIP.HdmDiAyAXwWnyJki0dd4xQHaLH?rs=1&pid=ImgDetMain" class="d-block w-50" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
